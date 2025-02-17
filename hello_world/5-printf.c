ff88120d40dcd89029181caa2839b8308ece24a2
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-17 05:37:59 +0000

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Description - Prints "with proper grammar, but the outcome is a piece of art,"
+ * Main - Prints "with proper grammar, but the outcome is a piece of art,"
  * Return - Return is 0 which indicates a successful runtime
 */
 int main(void)
