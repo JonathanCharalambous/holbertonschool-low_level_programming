cd28dae76492e9e2e2881d99bf6d0a94136ffb5d
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-17 07:18:16 +0000

    task 9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return: Always 1 (Error)
 */
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, strlen(quote) - 1);
-	return 1;
+	write(2, quote, sizeof(quote) - 1);
+	return (1);
 }
