21583e3b8468a499f5e1b6531f6eb0e76a0c9aa6
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-17 03:06:21 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
-int main(){
-
-
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilinigual puzzle");
 	return 0;
-
-
 }
