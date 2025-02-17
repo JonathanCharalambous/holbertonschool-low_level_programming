3dabe6a8da2903d638f548d7f4d6263052aac37b
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-17 06:00:53 +0000

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various primitive data types found in C
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long);
+	sizeof(double);
+	sizeof(float);
+	return (0);
+}
