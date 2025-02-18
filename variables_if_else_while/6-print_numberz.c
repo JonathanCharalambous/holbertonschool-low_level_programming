6bd5b85816d0cd3d2686031e18cb1f522c9aa111
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-18 11:09:53 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+	* main - Prints numbers 0 - 10
+	* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int n = 48;
+
+	while (n != 58)
+	{
+		putchar(n);
+		n += 1;
+	}
+	putchar('\n');
+	return (0);
+}
