f8482000943688fe9e1a1d4426ced34cfb9f5024
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-18 13:11:15 +0000

    task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+	* main - Prints all possible different
+	* combinations of two digits
+	* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int n1 = 48;
+	int n2 = 49;
+	int reduce = 8;
+
+	while (n1 != 57)
+	{
+		putchar(n1);
+		putchar(n2);
+		if (n1 == 56)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		if (n2 == 57)
+		{
+		    n1 += 1;
+		    n2 -= reduce;
+		    reduce -= 1;
+		}
+		n2 += 1;
+
+	}
+	putchar('\n');
+	return (0);
+}
