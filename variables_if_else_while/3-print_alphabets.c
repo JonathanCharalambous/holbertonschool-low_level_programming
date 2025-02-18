ed79a89cfac779a607334c10ef83cc4139387bb5
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-18 05:09:03 +0000

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+	* main - Prints the alphabet a-z and A-Z
+	* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a = 'a';
+	char bigA = 'A';
+
+	while (a != 'z' + 1)
+	{
+		putchar(a);
+		a += 1;
+	}
+	
+	while (bigA != 'Z' + 1)
+	{
+		putchar(bigA);
+		bigA += 1;
+	}
+	putchar('$');
+	return (0);
+}
