9f1f713158f3969e40f072fc469ffcacfe650c7a
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-18 05:52:27 +0000

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+	* main - Prints the alphabet a-z besides e and q
+	* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a != 'z' + 1)
+	{
+		if (a != 'q' || a != 'e')
+		{
+			putchar(a);
+			a += 1;
+		}
+		else
+		{
+			a += 1;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
