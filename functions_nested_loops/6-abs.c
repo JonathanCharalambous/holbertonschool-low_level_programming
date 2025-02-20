6834d3fdc5ef8cd0598f63a103ce6d44d0284adc
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-20 10:41:28 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+	* _abs - determines the absolute value of a variable c
+	* @c: The character that will be checked
+	* Return: returns the absolute value of a variable c
+*/
+
+int _abs(int c)
+{
+	return (abs(c));
+}
