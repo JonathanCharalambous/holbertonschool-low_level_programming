b11da4ba03a0506721c7bf87dbd623c12dbf37e7
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-20 10:10:47 +0000

    task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
-	* _islower - determines is a variable c is alpha or not
+	* _isalpha - determines is a variable c is alpha or not
 	* @c: The character that will be checked
 	* Return: 1 is lowercase 0 if not
 */
