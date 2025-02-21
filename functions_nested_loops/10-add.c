f5ef2a8fbdb58eb45a9e4461ef0898ca3ee42a22
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-21 07:12:00 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+	* add - Determines the sum of two given ints
+	* @i: The 1st int to be added
+	* @j: The 2nd int to be added
+	* Return: The sum of two ints i and j
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
