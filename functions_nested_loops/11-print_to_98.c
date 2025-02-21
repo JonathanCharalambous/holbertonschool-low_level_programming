a849c95f356b0508aeece768ef99536729ba5259
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-21 07:43:41 +0000

    task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+	* print_to_98: prints every number until the int n
+	* @n: The number we will print to
+	* Return: void
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d", n);
+			if (n < 97)
+			{
+				break;
+			}
+			printf(", ");
+			n++;
+		}
+	}
+
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			if (n > 97)
+			{
+				break;
+			}
+			printf(", ");
+			n--;
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+
+
+}
