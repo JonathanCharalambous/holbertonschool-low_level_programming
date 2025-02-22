3e9d47e7af5f1e43b9f3b2a9f0b9c5ece88c8015
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-22 05:47:11 +0000

    task 14

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * printFib - calculates the fibonacci sequence to
+ * the desired number
+ * @n: The number to which the sequence is calculated
+ * Return: void
+ */
+
+
+void printFib(int n)
+{
+	static long long int n1;
+	static long long int n2 = 1;
+	static long long int n3;
+
+	if (n == 0)
+		return;
+
+	n3 = n1 + n2;
+	printf("%lld ", n3);
+	n1 = n2;
+	n2 = n3;
+	printFib(n - 1);
+}
+
+
+/**
+ * main - prints the fibonacci sequence
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	printFib(50 - 2);
+
+	return (0);
+}
