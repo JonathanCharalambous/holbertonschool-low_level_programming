d0e654c45f56c1e0a8ca6673f573c8d46433f6ca
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-24 12:47:41 +0000

    task 12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -11,41 +11,21 @@
 void print_number(int n)
 {
 
-	int num = n;
-	int len = 0;
-
-	while (num != 0)
-	{
-		num = num / 10;
-		len++;
-	}
-
-	if (n == 0)
-		_putchar(n + '0');
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	if (len == 2)
+	if (num / 10)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		print_number(num / 10);
 	}
-
-	if (len == 3)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 10) / 10) + '0');
-		_putchar((n %  10) + '0');
-	}
-	if (len == 4)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 10) / 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n %  10) + '0');
-	}
+	_putchar((num % 10) + '0');
 }
