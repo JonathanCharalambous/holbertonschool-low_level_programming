041e17133e8f4d4a4038c0420ce7dad0d94028fb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-24 02:44:17 +0000

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * mul - multiples two variables
+ * @a: The first number to multiply
+ * @b: The first number to multiply
+ * Return: multiple of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
