7ecb67d5bf90a82c0df0f9f42c2a82c565ed77f0
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-24 03:56:19 +0000

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ * Return: void
+ */
+void print_numbers(void)
+{
+
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
