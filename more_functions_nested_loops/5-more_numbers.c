635b29e764e39935b67c76979f229f235090d2ae
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-24 04:41:48 +0000

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	for(int i = 0; i <= 10; i++)
+	{
+		for(int j = 0; j <= 14; j++)
+		{	if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+
+			}
+		}
+		
+		_putchar('\n');
+	}
+	
+}
