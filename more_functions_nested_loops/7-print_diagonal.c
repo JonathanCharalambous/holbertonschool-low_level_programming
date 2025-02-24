cf0b53384747680420168af134ce1e8f838777b5
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-24 06:27:22 +0000

    task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,8 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n > 0)
-	{	for (j = 0; j < n; j++)
+	{
+		for (j = 0; j < n; j++)
 		{
 			for (i = 0; i < j; i++)
 			{
@@ -24,5 +25,5 @@ void print_diagonal(int n)
 	}
 	else
 		_putchar('\n');
-	
+
 }
