69f545b039ef21084371f48905bd469a1908ded1
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-25 10:06:17 +0000

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - counts the size of a string s
+ * @s: string to check
+ * Return: length on variable s
+ */
+int _strlen(char *s)
+{
+	return(strlen(s));
+
+}
