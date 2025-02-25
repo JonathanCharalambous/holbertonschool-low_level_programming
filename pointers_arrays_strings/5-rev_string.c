8f6fc6591d92df477a5eb16571dce931199102cf
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-25 12:06:25 +0000

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -19,11 +19,11 @@ void rev_string(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	for(i = 0; i < len / 2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[len - i - 1];
-		s[len - i -1] = temp;
+		s[len - i - 1] = temp;
 	}
 
 }
