8e5aa9957cbecd48fd1d0f07582c23bc90bfe017
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-26 02:47:18 +0000

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_array - prints the array on ints
+ * @n: size of array
+ * @a: array of ints
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		printf("%d, ", a[i]);
+
+}
