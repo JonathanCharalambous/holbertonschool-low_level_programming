16bef50b0cb2bc9461c1c3e6ee388aa2cb3a7e77
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-26 01:18:42 +0000

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ * @str: string to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+
+	int i = 0;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+
+}
