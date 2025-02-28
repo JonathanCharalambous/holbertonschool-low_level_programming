42c7d564a9216d4552a1a6e3e9e6557bdd1258eb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-28 05:11:49 +0000

    pro3t6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+/**
+ * cap_string- uppercased the first letter of every word
+ * @s: string to uppercase
+ * Return: the intial string with the first letter of each word uppercased
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+			if (s[i] == separators[j])
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+
+
+		i++;
+	}
+
+	return (s);
+}
