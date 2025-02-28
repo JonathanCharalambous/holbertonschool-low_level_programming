2d296d6d75ce1cbf7dfb8c5c7df05a5259e52a1b
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-02-28 03:03:40 +0000

    pro3t5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+/**
+ * string_toupper- concats two string together
+ * @s: string to uppercase
+ * Return: the intial string with the src added
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+
+		i++;
+	}
+
+	return (s);
+}
