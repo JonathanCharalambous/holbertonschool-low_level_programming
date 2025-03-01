14ba452ac04c0b6c1b2dae0e75bca758df87d6ab
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-01 01:45:12 +0000

    pro3t7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+/**
+ * leet- converts a certain letters of a string with respective numbers
+ * @s: string to convert
+ * Return: the converted string
+ */
+
+char *leet(char *s)
+{
+	char letter[] = {'a', 'e', 'e', 't', 'l'};
+	char num[] = {4, 3, 0, 7, 1};
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == letter[j] || s[i] == letter[j] - 32)
+			{
+				s[i] = num[i] + '0';
+				break;
+			}
+		}
+
+		i++;
+
+	}
+
+
+	return (s);
+}
