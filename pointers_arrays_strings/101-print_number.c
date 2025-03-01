e078f7ef045e61472967a9aa7a41770019d1e4fb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-01 12:12:06 +0000

    pro3t101

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+/**
+ * print_number- concats two string together
+ * @n: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
