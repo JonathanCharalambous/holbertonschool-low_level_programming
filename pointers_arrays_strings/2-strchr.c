d3710aaafb03cc7fff251dd6fec1410c3d3bfccf
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-03 02:27:24 +0000

    pro4t2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,29 +9,20 @@
  * _strchr- concats two string together
  * @s: initial string
  * @c: the char we are looking for
- * Return: the string from the point of the found char
+ * Return: the string from the point of the found char or NULL
  */
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	int j = 0;
-	int k = strlen(s);
-	char *str  = malloc((k + 1) * sizeof(char));
 
-	while(s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			for (j = 0; i < k; i++, j++)
-				str[j] = s[i];
-			
-			str[j] = '\0';
-			return (str);
+			return s;	
 		}
-		
-		i++;
 
+		s++;
 	}
 	
 	return (NULL);
