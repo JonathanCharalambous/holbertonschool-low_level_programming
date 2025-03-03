1d770d5b35fe532dc9eb828e44472f469d29814e
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-03 06:14:47 +0000

    pro3t4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strspn- checks for the first char not in accept
+ * and counts the string len from there
+ * @s: the string we will check
+ * @accept: the chars discluded
+ * Return: number of characters from the first char not present
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+        int i = 0;
+
+        while (*s)
+        {
+
+                for (i = 0; accept[i] != '\0'; i++)
+                {
+                        if (*s == accept[i])
+                        {
+                                return (s);
+                        }
+                }
+
+
+                s++;
+        }
+
+        return (NULL);
+}
