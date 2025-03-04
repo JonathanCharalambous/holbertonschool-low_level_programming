1c754a30cdcc4d151227267362705e89c406301a
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-04 02:41:36 +0000

    pro3t100

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+
+/**
+ * set_string- sets a string to another using a var to
+ * @s: variable we want to set
+ * @to: variable we use to set
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+
+
+}
