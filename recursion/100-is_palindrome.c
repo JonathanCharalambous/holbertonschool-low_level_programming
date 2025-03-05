24a6b72b26d6cdc19a600052c05486a6a8f81ac9
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-05 08:29:11 +0000

    task 100

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -6,6 +6,45 @@
 
 
 /**
+ * _strcmp- comapares two string to check if they are equal
+ * @s1: first string to compare
+ * @s2: second string to command
+ * Return: 0 if strings are equal otherwise 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (1);
+		i++;
+	}
+
+	return (s1[i] == s2[i] ? 0 : 1);
+}
+
+/**
+ * _strlen- checks the length of a string
+ * @s: the string to check the length of
+ * Return: the length of the strong
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+
+	return (len);
+
+}
+
+/**
  * reverse_string- reverses our string s using recursion
  * @s: The string to reverse
  * @rev: Buffer to hold the reversed string
@@ -31,13 +70,13 @@ void reverse_string(char *s, char *rev, int index, int len)
  * @s: the string we will check
  * Return: 1 if s is a palindrome 0 if not
  */
- 
+
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
+	int len = _strlen(s);
 	char sRev[100];
 
 	reverse_string(s, sRev, 0, len);
 
-	return (strcmp(s, sRev) == 0);
+	return (_strcmp(s, sRev) == 0);
 }
