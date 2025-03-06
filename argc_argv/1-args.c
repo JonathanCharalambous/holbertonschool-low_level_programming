aa40905a1cc791db3b8cbb09f2e07f9fdc4d00fc
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-06 02:37:47 +0000

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main- prints the number of command line arguments
+ * @argc: Number of command line inputs
+ * @argv: Array of command line inputs
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+        
+	printf("%d\n", argc - 1);
+
+        return (0);
+}
