e85ba243a88b7aca832e7e09fa54c86b15932f1e
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-07 02:28:02 +0000

    task100

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -15,12 +15,16 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 	int count = 0;
-	int cents = atoi(argv[1]);
+	int cents = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	else
+	{
+		cents = atoi(argv[1]);
 	}
 
 	if (cents < 0)
