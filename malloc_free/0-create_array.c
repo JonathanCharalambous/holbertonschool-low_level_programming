027d20feacf062653ed83da3c4649353dec656d5
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-10 00:09:30 +0000

    task0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,6 +17,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *s = malloc(size * sizeof(char));
 
+	if (s == NULL)
+		return (NULL);
+
 	if (size == 0)
 		return (0);
 
