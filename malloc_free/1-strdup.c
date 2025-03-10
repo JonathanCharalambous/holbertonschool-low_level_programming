fe7493cbc309778003ec71bdc278b4da3e6c048a
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-10 00:37:43 +0000

    task1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strdup- creates an array of chars filled with a variable c
+ * @str: The string to copy
+ * Return: the copied string
+ */
+
+char *_strdup(char *str)
+{
+
+	unsigned int i = 0;
+	char *s = malloc((strlen(str) + 1)  * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	s[i] = '\0';
+
+	return (s);
+
+
+
+}
