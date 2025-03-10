1c2be06439f9cea647dbe2081dc7202200e518b7
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-10 00:07:17 +0000

    task0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,22 +5,27 @@
 
 
 /**
- * factorial- finds the factorial of the given n
- * @n: the number we will find the factorial of
- * Return: the factorial of n
+ * create_array- creates an array of chars filled with a variable c
+ * @size: the size of the array
+ * @c: the characters to fill the array
+ * Return: the filled array
  */
- 
-char *create_array (unsigned int size, char c)
+
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i = 0;
-    char *s = malloc(size * sizeof(char));
 
-    for (i = 0; i < size; i++)
-    {
-    	s[i] = c;
-    }
+	unsigned int i = 0;
+	char *s = malloc(size * sizeof(char));
 
-    return (s);
+	if (size == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
 
 
 
