3bc66a81b663dcaa799a6441ad985e40bd7eceae
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-11 10:51:45 +0000

    task2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,8 +14,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *arr = NULL;
-	unsigned int i = 0;
+	char *arr = NULL;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -25,8 +24,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size * nmemb; i++)
-		arr[i] = 0;
 
 	return (arr);
 }
