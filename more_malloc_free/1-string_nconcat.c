0d138271495bc552e1f8d71220aec5eacf458f6d
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-11 06:33:56 +0000

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,10 +32,14 @@ unsigned int _strlen(char *s)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str = malloc((_strlen(s1) * sizeof(char)) +
-			(_strlen(s2) * sizeof(char)) + 1);
+	char *str = malloc(( (_strlen(s1) + strlen(s2) + 1) * sizeof(char)));
 	unsigned int i = 0;
 	unsigned int j  = 0;
+
+	
+	
+	if (str == NULL)
+		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
