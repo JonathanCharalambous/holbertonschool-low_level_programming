2d463242ea1f6743e97edd1008d682c61e9ca4bc
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-12 14:08:36 +0000

    task2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,10 +14,10 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "nil";
+		d->name = "(nil)";
 
 	if (d->owner == NULL)
-		d->owner = "nil";
+		d->owner = "(nil)";
 
 
 
