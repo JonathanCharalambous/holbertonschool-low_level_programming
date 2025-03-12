9a217a7612587dbbe15a318c17dc70e07eeff64b
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-12 11:25:39 +0000

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+
+/**
+ * struct dog - define a struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
