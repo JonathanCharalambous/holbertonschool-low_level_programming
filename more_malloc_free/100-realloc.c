52050c07905a44115ef31cca516a063022ca7185
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-12 01:05:18 +0000

    task100

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	if (ptr == NULL)
+	if (ptr != NULL)
 	{
 		size = (old_size < new_size) ? old_size : new_size;
 
