edad5943b46fb2faba24c823d01e34ecffbf04bd
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-16 06:54:21 +0000

    task101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -42,53 +42,6 @@ int is_digit(char *s)
 }
 
 
-/**
- * multiply- multiplies the two digits which we have passed as strings
- * @n1: the first number
- * @n2: the second number
- * Return: void
- */
-
-void multiply(char *n1, char *n2)
-{
-	int len1 = _strlen(n1);
-	int len2 = _strlen(n2);
-	int *result;
-	int i, j, mul, carry, sum;
-
-	result = malloc(sizeof(int) * (len1 + len2));
-	if (!result)
-		errors();
-
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = 0;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			mul = (n1[i] - '0') * (n2[j] - '0');
-			sum = mul + result[i + j + 1] + carry;
-			result[i + j + 1] = sum % 10;
-			carry = sum / 10;
-		}
-		result[i + j + 1] += carry;
-	}
-
-	i = 0;
-	while (i < len1 + len2 && result[i] == 0)
-		i++;
-
-	if (i == len1 + len2)
-		_putchar('0');
-	else
-		for (; i < len1 + len2; i++)
-			_putchar(result[i] + '0');
-    
-	_putchar('\n');
-	free(result);
-}
 
 /**
  * main- multiplies two numbers given as arguments
@@ -99,9 +52,44 @@ void multiply(char *n1, char *n2)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		errors();
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	multiply(argv[1], argv[2]);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
