d1e789d62062b61b5274ac414b14aebb46285daa
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-17 02:25:47 +0000

    task2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index-iterates through an array to try and find a match with cmp
+ * @array: the array to iterate through
+ * @size: size of the array
+ * @cmp: function call to match an index with a desired value
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
