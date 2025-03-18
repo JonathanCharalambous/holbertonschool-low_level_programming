41b4b5f598d4cd8f610bc28a7b7ce2c07a69c52b
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-03-18 11:39:52 +0000

    task2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings- prints all words input as arguments
+ * @separator: the char to seperate each number
+ * @n: number of arguments
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *s = "";
+	unsigned int i = 0;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+
+		if (s == NULL)
+			printf("(nil)");
+
+		s = va_arg(args, char *);
+		printf("%s", s);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+
+	va_end(args);
+}
