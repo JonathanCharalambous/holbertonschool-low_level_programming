bc63347060869fbb3c628c90b00afc716d6022d0
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-01 05:25:58 +0000

    task3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+
+/**
+ * add_node_end- copies a string and adds it to the end of linked list
+ * @head: the head of the list
+ * @str: The string we want to duplicate and add
+ * Return: The head of the list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *temp;
+	size_t length = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (!new_node->str)
+		return (NULL);
+	while (*str)
+	{
+		length++;
+		str++;
+	}
+	new_node->len = length;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
