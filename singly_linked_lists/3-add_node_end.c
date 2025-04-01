4e5aa2c844ea2e9af3e8ab2b5425189b72296e97
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-01 05:36:55 +0000

    task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,21 +10,18 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
+	list_t *new_node, *temp;
 	size_t length = 0;
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
 	new_node->str = strdup(str);
 	if (!new_node->str)
-		return (NULL);
-	while (*str)
 	{
-		length++;
-		str++;
+		free(new_node);
+		return (NULL);
 	}
+	while (str[length])
+		length++;
 	new_node->len = length;
 	new_node->next = NULL;
 	if (*head == NULL)
