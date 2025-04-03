6a174e9b6b70c0e6d1d8b3eb30171c96ee91df40
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-03 06:40:21 +0000

    task4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,19 @@
 #include "lists.h"
-
+/**
+ * free_dlistint- frees up head of a list
+ * @head: the head of a list
+ * Returm: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *t;
-	
+
 	while (head)
 	{
 		t = head->next;
 		free(head);
 		head = t;
 	}
-	
-	
+
+
 }
