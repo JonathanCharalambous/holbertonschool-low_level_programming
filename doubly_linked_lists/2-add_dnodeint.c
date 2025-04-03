41a4539a5fddc709a9d748ae2f9cb09543501077
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-03 01:43:10 +0000

    task2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint- prints all elements of a dlistint_t list
+ * @head: the head of the list
+ * @n: the number to pass
+ * Return: The head of the list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	new_node->n = n;
+
+	new_node->next = (*head);
+
+	new_node->prev = NULL;
+
+	if ((*head) != NULL)
+		(*head)->prev = new_node;
+
+	(*head) = new_node;
+
+	return (*head);
+
+
+}
