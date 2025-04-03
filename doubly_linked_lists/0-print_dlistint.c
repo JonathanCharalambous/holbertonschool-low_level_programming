ede31cfe5d1af75f5b93057e9405606efff997b1
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-03 01:10:30 +0000

    task0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_distinct- prints all elements of a dlistint_t list
+ * @h: the head of the list
+ * Return: The number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+
+	}
+
+	return (n);
+
+
+}
