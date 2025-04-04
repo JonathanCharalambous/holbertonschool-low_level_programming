ccb62588864d322365ee7b58af5f4a02649819bb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-04 04:24:47 +0000

    task4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -8,14 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index >= 64)
 		return (-1);
 
-	if ((*n >> index) & 1)
-	{
-		*n &= ~(1UL << index);
-		return (1);
-	}
+	*n &= ~(1UL << index);
 
-	return (-1);
+	return (1);
 }
