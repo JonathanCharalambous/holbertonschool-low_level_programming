5ec43d6a9a79b55c86df9e3d2e232d8086129a12
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-06 03:31:32 +0000

    task2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text to the end of a file
+ * append_text_to_file- appends text to the end of a file
  * @filename: the name of the file
  * @text_content: content to append
- * Return : 1 if successful or -1 if not 
+ * Return: 1 if successful or -1 if not
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -28,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		len++;
 
 	wr = write(fd, text_content, len);
-	if(wr == -1 || wr != len)
+	if (wr == -1 || wr != len)
 		return (-1);
 	close(fd);
 	return (1);
